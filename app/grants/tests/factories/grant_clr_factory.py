0f9d38f45c0763a7519b889f2dc4327ffdf0e900
Kevin Solorio <kevin.solorio@hey.com>
2021-11-07 11:31:27 -0600

    update factories and tests

--- app/grants/tests/factories/grant_clr_factory.py
+++ app/grants/tests/factories/grant_clr_factory.py
@@ -9,12 +9,10 @@ from dashboard.tests.factories import ProfileFactory
 
 @pytest.mark.django_db
 class GrantCLRFactory(factory.django.DjangoModelFactory):
-    """Create mock GrantCLR for testing."""
-
     class Meta:
         model = GrantCLR
 
-    round_num = 2
-    start_date = datetime.now()
-    end_date = start_date + timedelta(weeks=2)
+    round_num = factory.Faker('pyint')
+    start_date = factory.LazyFunction(datetime.now)
+    end_date = factory.LazyAttribute(lambda o: o.start_date + timedelta(weeks=2))
     owner = factory.SubFactory(ProfileFactory)
